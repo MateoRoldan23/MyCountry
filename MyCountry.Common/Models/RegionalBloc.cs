133066fcd16d666732b6d01d3551f6a3483f9fc1
MATEO ROLDAN ZAPATA <mateoroldan183493@correo.itm.edu.co>
2019-10-14 18:32:36 -0500

    Create Services

---
+++ MyCountry.Common/Models/RegionalBloc.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace MyCountry.Common.Model
+{
+    public class RegionalBloc
+    {
+        [JsonProperty("acronym")]
+        public string Acronym { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("otherAcronyms")]
+        public object[] OtherAcronyms { get; set; }
+
+        [JsonProperty("otherNames")]
+        public object[] OtherNames { get; set; }
+    }
+}
